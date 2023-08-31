70ab12be203fd62dc6f6d926a8d9d12becadaca7
Vimal Raveendran <vimalraveendra@gmail.com>
2023-08-31 22:46:01 +0200

    added pwa files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React,{useState,useEffect} from "react";
-import SearchBox from "./Components/SearchBox/SearchBox";
-import CardList from "./Components/CardList/CardList";
+import SearchBox from "./components/SearchBox/SearchBox.component";
+import CardList from "./components/CardList/CardList.component";
 import "./App.css";
+
+
 const App =()=> {
   const [searchText,setSearchText]= useState("")
   const [monsters,setMonsters] = useState([])
   
-  useEffect(()=>{
-    const getMonsters = async()=>{
-      const request = await fetch(`https://jsonplaceholder.typicode.com/users`);
-      const  data= await request.json();
-      setMonsters(data)
-    }
-    getMonsters();
-    return {
-      
-    }
-  },[])
- 
 
   const  handleOnChangeText= (e) => {
     let target = e.target.value;
@@ -28,6 +18,21 @@ const App =()=> {
       (monster) =>
         monster.name.toLowerCase().indexOf(searchText.toLowerCase().trim()) > -1
     );
+
+  useEffect(()=>{
+    const getMonsters = async()=>{
+      try{
+      const request = await fetch(`https://jsonplaceholder.typicode.com/users`);
+      const  data= await request.json();
+      setMonsters(data)
+    }catch(error){
+      console.log(error)
+    }
+  }
+    getMonsters();
+ 
+  
+  },[])
 
     return (
       <div className="App">
